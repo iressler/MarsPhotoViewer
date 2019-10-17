44d511c97af882ee89da7d51bbca884670975823
Isaac Ressler <isaacressler@hotmail.com>
2019-10-17 14:54:30 -0700

    Initial Commit

---
+++ MarsPhotoViewer/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MarsPhotoViewer
+//
+//  Created by Isaac Ressler on 10/17/19.
+//  Copyright © 2019 Isaac Ressler. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MarsPhotoViewer/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MarsPhotoViewer
+//
+//  Created by Isaac Ressler on 10/17/19.
+//  Copyright © 2019 Isaac Ressler. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
